a31097538d4208413492279e8ac015f933db062d
admin <1>
2022-07-22 11:26:50 +0500

    Per commit, import git django

--- djangoProj/djangoProj/views.py
+++ djangoProj/djangoProj/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 def firstPage(request):
     a='<h1>Hello World!</h1>'
-    return render(request, '/index.html')
+    text = "Новый текст"
+    return render(request, './index.html',{
+        'a':a,
+        'text':text
+    })
