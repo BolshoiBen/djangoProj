4b744c74768bef979639309496171d47671c7f89
admin <1>
2022-07-29 17:50:11 +0500

    модели джанго

--- djangoProj/crm/admin.py
+++ djangoProj/crm/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Order       #Регистрация модели в админ панели
 # Register your models here.
+admin.site.register(Order)      #Регистрация модели в админ панели
--- djangoProj/crm/models.py
+++ djangoProj/crm/models.py
@@ -4,10 +4,23 @@ from django.db import models
 #здесь опичываются таблички базы данных
 class Order(models.Model):
     order_dt = models.DateTimeField(auto_now=True)
-    order_name=models.CharField(max_length=200)
-    order_phone=models.CharField(max_length=200)
+    order_name=models.CharField(max_length=200, verbose_name='Имя')
+    order_phone=models.CharField(max_length=200,verbose_name='Телефон')
+
+    def __str__(self):          #функция изменения типа отображения строки
+        return self.order_name
+
+    class Meta:                 #Суб класс мета для изменения отображение имени обьекта
+                                    #Воспользуемся строковым отображением типа данных
+        verbose_name = 'Заказ'          #единственное число
+        verbose_name_plural = 'Заказы'  #множественное число представления обьекта
+'''
+    python manage.py runserver
+'''
+
 
 '''
+    
 Работа с моделями в консоли
 
     python manage.py shell #открыть консоль
@@ -18,7 +31,7 @@ class Order(models.Model):
         connection.queries
         n3 = order.objects.create(order_name = 'bob', order_phone = '+7355558383')
         Order.objects.all()
-        Order.objects.create(order_name = 'bob', order_phone = '+7353531861')
+        Order.objects.create(order_name = 'bob', order_phone = '+7353531861') 
         Order.objects.filter(order_name= "bob"
         Order.objects.objects.order_by('id')
         Order.objects.objects.order_by('-id')
@@ -31,4 +44,10 @@ class Order(models.Model):
         order1.order_dt
         order1.order_phone = '23232'
         order1.save()
+        exit()
 '''
+
+"""
+    Панель админа
+        python manage.py createsuperuser
+"""
