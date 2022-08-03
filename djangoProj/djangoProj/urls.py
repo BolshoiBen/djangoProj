c43765de2360f81efeda80cb409d79d81405b28e
admin <1>
2022-08-03 15:17:16 +0500

    статические и медиа директории

--- djangoProj/djangoProj/urls.py
+++ djangoProj/djangoProj/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from crm import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.first_page),
-    path('thanks/',views.thank, name = "thann"),
-]
+    path('thanks/',views.thank, name = "thann")
+] + static(settings.MEDIA_URL, dociment_root=settings.MEDIA_ROOT)
 
